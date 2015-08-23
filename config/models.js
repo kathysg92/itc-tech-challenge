0f67f44f3c4a0be57e7d4efdcfc278b1c3ebe665
ad_serranog <ad_serranog@vmtstkat007.amr.corp.intel.com>
2015-08-23 14:28:07 -0700

    Rita history

--- config/models.js
+++ config/models.js
@@ -17,7 +17,8 @@ module.exports.models = {
   * connections (see `config/connections.js`)                                *
   *                                                                          *
   ***************************************************************************/
-  connection: 'kadTechChallenge',
+  // connection: 'kadTechChallenge',
+  connection: 'katTesting',
 
   /***************************************************************************
   *                                                                          *
